d1cbfc472b0b344ae92491074bed1c8ecc9b12cd
xiao.gong <xiao.gong@17zuoye.com>
2019-12-20 17:11:39 +0800

    new g2o lib fit

--- ch6/g2o_curve_fitting/main.cpp
+++ ch6/g2o_curve_fitting/main.cpp
@@ -71,13 +71,18 @@ int main( int argc, char** argv )
         cout<<x_data[i]<<" "<<y_data[i]<<endl;
     }
     
+    typedef g2o::BlockSolver< g2o::BlockSolverTraits<3,1> > Block;
+Block::LinearSolverType* linearSolver = new g2o::LinearSolverDense<Block::PoseMatrixType>();
+Block* solver_ptr = new Block( std::unique_ptr<Block::LinearSolverType>(linearSolver) );
+g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg(std::unique_ptr<Block>(solver_ptr) );
+
     // 构建图优化，先设定g2o
-    typedef g2o::BlockSolver< g2o::BlockSolverTraits<3,1> > Block;  // 每个误差项优化变量维度为3，误差值维度为1
-    Block::LinearSolverType* linearSolver = new g2o::LinearSolverDense<Block::PoseMatrixType>(); // 线性方程求解器
-    Block* solver_ptr = new Block( linearSolver );      // 矩阵块求解器
-    // 梯度下降方法，从GN, LM, DogLeg 中选
-    g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg( solver_ptr );
-    // g2o::OptimizationAlgorithmGaussNewton* solver = new g2o::OptimizationAlgorithmGaussNewton( solver_ptr );
+//     typedef g2o::BlockSolver< g2o::BlockSolverTraits<3,1> > Block;  // 每个误差项优化变量维度为3，误差值维度为1
+//     Block::LinearSolverType* linearSolver = new g2o::LinearSolverDense<Block::PoseMatrixType>(); // 线性方程求解器
+//    Block* solver_ptr = new Block( linearSolver );      // 矩阵块求解器
+//     // 梯度下降方法，从GN, LM, DogLeg 中选
+//     g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg( solver_ptr );
+//     // g2o::OptimizationAlgorithmGaussNewton* solver = new g2o::OptimizationAlgorithmGaussNewton( solver_ptr );
     // g2o::OptimizationAlgorithmDogleg* solver = new g2o::OptimizationAlgorithmDogleg( solver_ptr );
     g2o::SparseOptimizer optimizer;     // 图模型
     optimizer.setAlgorithm( solver );   // 设置求解器
